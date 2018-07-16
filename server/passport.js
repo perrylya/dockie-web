e89e60344f2b4bf159737fda32d436aaa3940b39
perrylya <perrylya@gmail.com>
2018-07-16 15:04:38 -0700

    created passport

---
+++ server/passport.js
@@ -0,0 +1,76 @@
+import passport from 'passport';
+import LocalStrategy from 'passport-local'.Strategy;
+
+import models from './models/models'
+let User = models.User;
+
+let app = express();
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  User.findOne({ username: username }, function (err, user) {
+    // if there's an error, finish trying to authenticate (auth failed)
+    if (err) {
+      console.log(err);
+      return done(err);
+    }
+    // if no user present, auth failed
+    if (!user) {
+      console.log(user);
+      return done(null, false);
+    }
+    // if passwords do not match, auth failed
+    if (user.password !== password) {
+      return done(null, false);
+    }
+    // auth has has succeeded
+    return done(null, user);
+  });
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', auth(passport));
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
