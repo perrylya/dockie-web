ff28b1acf29ec4a5d8b7206fd540451e120b7585
Francis Kigawa <francis.kigawa@yale.edu>
2018-07-17 13:14:03 -0700

    modal

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -29,13 +29,17 @@ export default class Documents extends React.Component {
       docId: ''
     };
 
+    this.openNewDocModal = this.openNewDocModal.bind(this);
     this.openModal = this.openModal.bind(this);
     this.afterOpenModal = this.afterOpenModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
   }
 
+  openNewDocModal() {
+    this.setState({modalIsOpen: true});
+  }
+
   openModal() {
-    console.log(this.state.existingDocs)
     this.setState({modalIsOpen: true});
   }
 
@@ -74,7 +78,17 @@ export default class Documents extends React.Component {
           Create/Edit Documents
         </h2>
         <button onClick={() => this.props.redirect('CreateDoc')}>Create New Document</button>
-
+        {/* <button onClick={this.openNewDocModal}>Create New Doc</button>
+        <Modal isOpen={this.state.modalIsOpen} style={customStyles}>
+          <h2 ref={subtitle => this.subtitle = subtitle}>New Doc</h2>
+          <form>
+            <div>
+              New Document
+            </div>
+          </form>
+          <button onClick={this.addModal}>Create New</button>
+          <button onClick={this.closeModal}>Cancel</button>
+        </Modal> */}
 
         <button onClick={this.openModal}>Edit Existing Doc</button>
         <Modal isOpen={this.state.modalIsOpen} style={customStyles}>
