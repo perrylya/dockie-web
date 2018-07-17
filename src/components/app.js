29057de60b9246bcea3606204fb54b66f6f58d21
Francis Kigawa <francis.kigawa@yale.edu>
2018-07-16 18:31:29 -0700

    all

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Documents from './Documents.js';
 import CreateDoc from './CreateDoc.js';
 import LoginScreen from './Login';
-import DocumentsStyle from './DocumentsStyle'
 
 class App extends React.Component {
   constructor(props) {
