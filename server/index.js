201d13f98464c54711248ee93f35e5cfb0f4b297
asiopongco <asiopongco@gmail.com>
2018-07-17 11:31:40 -0700

    edited index to include routes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 let app = express();
+import routes = from './auth.js';
 import mongoose from 'mongoose';
+import LocalStrategy from 'passport-local'
+var MongoStore = require('connect-mongo')(session);
 
 mongoose.connection.on('connected', () =>{
   console.log('Successfully connected to MongoDB');
@@ -17,5 +20,19 @@ mongoose.connect(process.env.MONGODB_URI);
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
+
+//Passport Implementation
+app.use(session({
+  secret: 'yer',
+  store: new MongoStore({mongooseConnection: require('mongoose').connection})
+}));
+
+app.use('/', routes);
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 app.listen(8888);
